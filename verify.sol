853f307aa0f1fb012768b2d46e787bc314e1932c
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2022-01-13 10:11:04 +0700

    Update verify.sol

--- verify.sol
+++ verify.sol
@@ -60,14 +60,6 @@ contract verifySignature{
         require(sig.length == 65, "invalid signature length");
 
         assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
 
             // first 32 bytes, after the length prefix
             r := mload(add(sig, 32))
@@ -77,6 +69,5 @@ contract verifySignature{
             v := byte(0, mload(add(sig, 96)))
         }
 
-        // implicitly return (r, s, v)
     }
 }
